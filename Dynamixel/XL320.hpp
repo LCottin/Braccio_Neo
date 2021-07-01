f6a314067b4611f0c28951176ded79cf77203bbb
LCottin <cottin_leonard@icloud.com>
2021-07-01 15:32:55 +0200

    Added inherited

--- Dynamixel/XL320.hpp
+++ Dynamixel/XL320.hpp
@@ -6,41 +6,25 @@
 #include <iostream>
 
 #include "functions.hpp"
+#include "Motor.hpp"
 #include "include/dynamixel_sdk/dynamixel_sdk.h"  
 
 using namespace std;
 using namespace dynamixel;
 
-class XL320
+class XL320 : public Motor
 {
     private:
-        short _AddrTorque;
-        short _AddrGoalPos;
-        short _AddrPresentPos;
-
-        double _Protocol;
-        unsigned char _ID;
-
-        int _Baudrate;
-        string _PortName;
-
-        bool _TorqueEnable;
-        short _MinPos;
-        short _MaxPos;
-        short _PresentPos;
-        short _GoalPos;
-        short _Threshold;
-
-        uint8_t _Error;
-
-        PortHandler *portHandler;
-        PacketHandler *packetHandler;
 
     public:
         XL320(const unsigned char ID);
-        void init();
 
-        void move(unsigned short newPos);
+        void start();
+        bool openPort();
+        bool move(unsigned short newPos);
+        bool enableTorque();
+        bool disableTorque();
+        bool setBaudrate(const int baudrate);
 
         ~XL320();
 };
