c6f4e550cd36f57b2a5e99ff037249bd726c173a
LCottin <cottin_leonard@icloud.com>
2021-07-05 10:06:33 +0200

    Added sleep between each movment

--- Dynamixel/main.cpp
+++ Dynamixel/main.cpp
@@ -1,15 +1,19 @@
 #include "XL320.hpp"
+#include <unistd.h>
 
 int main(int argc, char const *argv[])
 {
     XL320 moteur(atoi(argv[1]));
+
     moteur.move(1000);
     moteur.setLed(YELLOW);
+    sleep(1);
     moteur.move(0);
     moteur.setLed(BLUE);
+    sleep(1);
     moteur.move(500);
     moteur.setLed(RED);
-
+    sleep(1);
     moteur.move(0);
     moteur.setSpeed(200);
     cout << "Current pos : " << moteur.getPosition() << endl;
