cd3f6b8c879820ebf87493277da1f0c87a5235f4
LCottin <cottin_leonard@icloud.com>
2021-07-06 11:51:57 +0200

    Created AX18A class

---
+++ Dynamixel/AX18A.hpp
@@ -0,0 +1,39 @@
+#ifndef __AX18A__
+#define __AX18A__
+
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+#include "functions.hpp"
+#include "Motor.hpp"
+#include "include/dynamixel_sdk/dynamixel_sdk.h"  
+
+using namespace std;
+using namespace dynamixel;
+
+class AX18A : public Motor
+{
+    private:
+        bool start();
+        bool openPort();
+        bool setBaudrate(const unsigned baudrate);
+        
+    public:
+        AX18A(const unsigned char ID);
+
+        bool enableTorque();
+        bool disableTorque();
+        bool move(const unsigned newPos, const bool blocking = false, const bool debug = false);
+        bool setSpeed(const unsigned speed);
+        bool ledOn();
+        bool ledOff();
+        double getVoltage();
+        double getTemperature();
+        double getLoad();
+        bool Infos();
+
+        ~AX18A();
+};
+
+#endif
