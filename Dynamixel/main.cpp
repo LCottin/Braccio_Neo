53e7951b377fe7123378d38e07aa5f72127e2de8
LCottin <cottin_leonard@icloud.com>
2021-07-06 10:42:14 +0200

    Changed UUT

--- Dynamixel/main.cpp
+++ Dynamixel/main.cpp
@@ -4,6 +4,7 @@
 
 int main(int argc, char const *argv[])
 {
+	/*
     XL320 moteur(atoi(argv[1]));
 
     moteur.move(1000, true);
@@ -26,28 +27,30 @@ int main(int argc, char const *argv[])
 
     cout << "Current pos : " << moteur.getPosition() << endl;
     moteur.Infos();
+	*/
 
-    // MX28AT moteur2(atoi(argv[1]));
+     MX28AT moteur2(atoi(argv[1]));
 
 
-    // moteur2.move(1000, true);
-    // moteur2.ledOff();
+     moteur2.move(1000, true);
+     moteur2.ledOff();
 
-    // moteur2.move(0, true);
-    // moteur2.ledOn();
-    // moteur2.move(500, true);
-    // moteur2.ledOff();
+     moteur2.move(0, true);
+     moteur2.ledOn();
+     moteur2.move(500, true);
+     moteur2.ledOff();
 
-    // moteur2.move(0, true);
-    // moteur2.setSpeed(200);
-    // moteur2.move(2047);
-    // cout << "Current pos : " << moteur2.getPosition() << endl;
-    // moteur2.disableTorque();
+     moteur2.move(0, true);
+     moteur2.setSpeed(200);
+     moteur2.move(2047);
+     cout << "Current pos : " << moteur2.getPosition() << endl;
+     moteur2.disableTorque();
 
-    // if (moteur2.getTorque())
-    //     cout << "Torque enabled" << endl;
-    // else
-    //     cout << "Torque disabled" << endl;
+     if (moteur2.getTorque())
+         cout << "Torque enabled" << endl;
+     else
+         cout << "Torque disabled" << endl;
 
-    // cout << "Current pos : " << moteur2.getPosition() << endl;
+     cout << "Current pos : " << moteur2.getPosition() << endl;
+     moteur2.Infos();
 }
