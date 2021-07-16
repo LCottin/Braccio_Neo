6c9647536fec6e0e91e4af4c0fdc3231545d64c1
LCottin <cottin_leonard@icloud.com>
2021-07-16 11:08:19 +0200

    Added class tests

--- Dynamixel/src/main.cpp
+++ Dynamixel/src/main.cpp
@@ -15,10 +15,6 @@ RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24, BCM2835_SPI_SPEED_16MHZ);
 //RF24 radio(RPI_V2_GPIO_P1_22, RPI_V2_GPIO_P1_24);
 RF24Network network(radio);
 
-#endif
-
-using namespace std;
-
 struct x
 {
 	short ID;
@@ -29,128 +25,16 @@ struct x
 const uint16_t noeudMere = 00;
 const uint16_t monNoeud = noeudMere;
 
+#endif
+
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    
-   /* AX12A moteur(3);
-
-    moteur.move(1000, true);
-    moteur.move(2000, true);
-    moteur.move(3000, true);
-
-    moteur.move(1000, true);
-    moteur.middle();
-    cout << "Current pos : " << moteur.getPosition() << endl;
-   // moteur.disableTorque();
-
-    if (moteur.getTorque())
-        cout << "Torque enabled" << endl;
-    else
-        cout << "Torque disabled" << endl;
-
-   // cout << "Current pos : " << moteur.getPosition() << endl;
-    moteur.Infos();
-	
-
-    //  MX28AT moteur2(atoi(argv[1]));
-
-    //  moteur2.move(1000, true);
-    //  moteur2.ledOff();
-
-    //  moteur2.move(0, true);
-    //  moteur2.ledOn();
-    //  moteur2.move(500, true);
-    //  moteur2.ledOff();
-
-    //  moteur2.move(0, true);
-    //  moteur2.setSpeed(200);
-    //  moteur2.move(2047);
-    //  cout << "Current pos : " << moteur2.getPosition() << endl;
-    //  moteur2.disableTorque();
-
-    //  if (moteur2.getTorque())
-    //      cout << "Torque enabled" << endl;
-    //  else
-    //      cout << "Torque disabled" << endl;
-
-    //  cout << "Current pos : " << moteur2.getPosition() << endl;
-    //  moteur2.Infos();
-
-    // MX64AT moteur3(atoi(argv[1]));
-
-    // moteur3.move(1000, true);
-    // moteur3.ledOff();
-
-    // moteur3.move(0, true);
-    // moteur3.ledOn();
-    // moteur3.move(500, true);
-    // moteur3.ledOff();
-
-    // moteur3.move(0, true);
-    // moteur3.setSpeed(200);
-    // moteur3.move(2047);
-    // cout << "Current pos : " << moteur3.getPosition() << endl;
-    // moteur3.disableTorque();
-
-    // if (moteur3.getTorque())
-    //     cout << "Torque enabled" << endl;
-    // else
-    //     cout << "Torque disabled" << endl;
-
-    // cout << "Current pos : " << moteur3.getPosition() << endl;
-    // moteur3.Infos();
-
-    // AX12A moteur4(atoi(argv[1]));
-
-    // moteur4.move(1000, true);
-    // moteur4.ledOff();
-
-    // moteur4.move(0, true);
-    // moteur4.ledOn();
-    // moteur4.move(500, true);
-    // moteur4.ledOff();
-
-    // moteur4.move(0, true);
-    // moteur4.setSpeed(200);
-    // moteur4.move(2047);
-    // cout << "Current pos : " << moteur4.getPosition() << endl;
-    // moteur4.disableTorque();
-
-    // if (moteur4.getTorque())
-    //     cout << "Torque enabled" << endl;
-    // else
-    //     cout << "Torque disabled" << endl;
-
-    // cout << "Current pos : " << moteur4.getPosition() << endl;
-    // moteur4.Infos();
-
-    MX106 moteur5(atoi(argv[1]));
-
-    moteur5.move(1000, true);
-    moteur5.ledOff();
-
-    moteur5.move(0, true);
-    moteur5.ledOn();
-    moteur5.move(500, true);
-    moteur5.ledOff();
-
-    moteur5.move(0, true);
-    moteur5.setSpeed(200);
-    moteur5.move(2047);
-    cout << "Current pos : " << moteur5.getPosition() << endl;
-    moteur5.disableTorque();
-
-    if (moteur5.getTorque())
-        cout << "Torque enabled" << endl;
-    else
-        cout << "Torque disabled" << endl;
-
-    cout << "Current pos : " << moteur5.getPosition() << endl;
-    moteur5.Infos();
-    */
-
-
     Braccio braccio;
+
+    braccio.stand();
+    braccio.moveAll(2000, 2000, 2000, 1000);
 
 /*
 
